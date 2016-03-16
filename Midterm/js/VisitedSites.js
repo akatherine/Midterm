b9aa78caf06a29fdbdfbc2b55846f1041e5c8dfc
akatherine <akatherine@archaeologist.com>
2016-03-16 14:23:04 -0500

    Added files via upload

--- Midterm/js/VisitedSites.js
+++ Midterm/js/VisitedSites.js
@@ -1,13 +1,20 @@
 function addmypopup(feature, layer){
- html = feature.properties.Site_Name + '<br>' + feature.properties.State + ", " + feature.properties.Country +  ", " +feature.properties.Time_perio + '<br>' +'<div minWidth: 500px><img src = "'+ feature.properties.ImagePath +'"></div>';
+ html = feature.properties.Site_Name + '<br>' + feature.properties.State + ", " + feature.properties.Country +  ", " +feature.properties.Time_perio + '<br>' +'<img src = "'+ feature.properties.ImagePath +'"></div>';
 layer.bindPopup(html);
 }
 
-var map = L.map('map').setView([17,-90], 6);
+var map = L.map('map').setView([19,-89], 6);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var satellite = L.tileLayer( 'http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+	type: 'sat',
+	ext: 'jpg',
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency',
+	subdomains: '1234'
+});
 
 
 
@@ -21,5 +28,6 @@ function( geojsonFeature ) {
 
 
 }).addTo(map);
+
 
 });
